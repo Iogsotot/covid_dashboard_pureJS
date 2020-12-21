516728a3a681ec011f6f1e53b6899305ae379135
Iogsotot <ajustusa@gmail.com>
2020-12-22 02:04:40 +0300

    WIP: swipe stats for countries list

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import '../styles/style.scss';
 import isoCountries from './ISOCountries';
 
@@ -22,19 +23,17 @@ export default class App {
 
   async createCountriesList() {
     const countriesListEl = document.querySelector('#countriesList');
+
     this.perCountryStats = await this.stats.getAllCountryStats();
     // this.countriesList = Object.keys(isoCountries);
     this.countriesList = Object.keys(this.perCountryStats);
-
-    console.log(this.perCountryStats[0]);
-    console.log(this.perCountryStats[0].active);
 
     for (let i = 0; i < this.countriesList.length; i++) {
       const countryTemplate = document.createElement('li');
       countryTemplate.innerHTML = `
         <li class="countries__item">
           <span class="country__name">${this.perCountryStats[i].country}</span>
-          <span class="country__cases--All-Abs">${this.perCountryStats[i].cases}</span>
+          <span class="country__cases--All-Abs show">${this.perCountryStats[i].cases}</span>
           <span class="country__active--All-Abs">${this.perCountryStats[i].active}</span>
           <span class="country__deaths--All-Abs">${this.perCountryStats[i].deaths}</span>
           <span class="country__recovered--All-Abs">${this.perCountryStats[i].recovered}</span>
@@ -56,7 +55,33 @@ export default class App {
       countriesListEl.innerHTML += countryTemplate.innerHTML;
     }
     // console.log(Object.keys(isoCountries));
+    this.countriesListDataControl();
   }
+
+  countriesListDataControl() {
+    const countriesListDataNameEl = document.querySelector('#countriesListDataName');
+    const arrowLeftEl = document.querySelector('#arrowLeft');
+    const arrowRightEl = document.querySelector('#arrowRight');
+    this.statsList = document.querySelectorAll('.countries__list span');
+
+    console.log(this.statsList);
+    arrowLeftEl.addEventListener('click', this.statsSwipe('left'));
+    arrowRightEl.addEventListener('click', this.statsSwipe('right'));
+  }
+
+  statsSwipe(direction) {
+    // собрать коллекцию всех li 
+    // итерироваться по всем спанам внутри li
+    // на right тогглить спану класс .show (i и i+1)
+    // при достижении конца списка - обнулять i
+    if (direction === 'right') {
+      for(const i = 0; i <  this.statsList.length; i += 1) {
+
+      }
+    }
+    // аналогично для left
+  }
+
 }
 
 const app = new App();

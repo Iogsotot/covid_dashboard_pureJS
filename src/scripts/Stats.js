d918726b9f476c3b04ead04bcb7c1cf06f2b59fb
Iogsotot <ajustusa@gmail.com>
2020-12-22 00:14:54 +0300

    feat: add dynamic countries list with all data & flag

--- src/scripts/Stats.js
+++ src/scripts/Stats.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
+import isoCountries from './ISOCountries';
+
 export default class Stats {
-  constructor(countryName = 'Russia') {
-    this.countryName = countryName;
+  constructor() {
     this.urls = {
       perCountryData: 'https://corona.lmao.ninja/v2/countries',
       totalData: 'https://corona.lmao.ninja/v2/all',
-      worldTimeline: 'https://disease.sh/v3/covid-19/historical/all?lastdays=all',
-      countryTimeline: `https://disease.sh/v3/covid-19/historical${`/${this.countryName}`}?lastdays=all`,
       allCountriesTimeline: 'https://disease.sh/v3/covid-19/historical?lastdays=all',
     };
   }
@@ -25,57 +24,51 @@ export default class Stats {
     return covidData;
   }
 
-  async getPerCountryStats(countryName = 'Angola') {
+  async getAllCountryStats() {
     const url = this.urls.perCountryData;
     const covidData = await this.getDataFromUrl(url);
-    const countries = [];
-    function getCountries() {
-      for (let i = 0; i < Object.keys(covidData).length; i += 1) {
-        countries[i] = covidData[i].country;
-      }
-      return countries;
-    }
+    // const countries = [];
+    // function getCountries() {
+    //   for (let i = 0; i < Object.keys(covidData).length; i += 1) {
+    //     countries[i] = covidData[i].country;
+    //   }
+    //   return countries;
+    // }
+    // const countryIndex = getCountries().indexOf(countryName);
 
-    const countryIndex = getCountries().indexOf(countryName);
-    // console.log('++++++++++++++++++++++++++++++++++++++++++++++');
-    // console.log(countryName);
-    // console.log(countryIndex);
-    // console.log(getCountries());
-    // console.log('++++++++++++++++++++++++++++++++++++++++++++++');
-
-    const covidDataPerCountries = {
-      countries,
-      countryISO: covidData[countryIndex].countryInfo.iso2,
-      countryName: covidData[countryIndex].country,
-      flag: covidData[countryIndex].countryInfo.flag,
-      cases: covidData[countryIndex].cases,
-      deaths: covidData[countryIndex].deaths,
-      recovered: covidData[countryIndex].recovered,
-      casesPerOneHundredThousand: Math.round(covidData[countryIndex].casesPerOneMillion / 10),
-      deathsPerOneHundredThousand: Math.round(covidData[countryIndex].deathsPerOneMillion / 10),
-      recoveredPerOneHundredThousand: Math.round(covidData[countryIndex].recoveredPerOneMillion
-        / 10),
-      todayCases: covidData[countryIndex].todayCases,
-      todayDeaths: covidData[countryIndex].todayDeaths,
-      todayRecovered: covidData[countryIndex].todayRecovered,
-      todayCasesPerOneHundredThousand: ((covidData[countryIndex].todayCases
-        / covidData[countryIndex].population) * 100000).toFixed(3),
-      todayDeathsPerOneHundredThousand: ((covidData[countryIndex].todayDeaths
-        / covidData[countryIndex].population) * 100000).toFixed(3),
-      todayRecoveredPerOneHundredThousand: ((covidData[countryIndex].todayRecovered
-        / covidData[countryIndex].population) * 100000).toFixed(3),
-      updated: (new Date(covidData[countryIndex].updated)).toDateString(),
-    };
+    // const covidDataPerCountries = {
+    //   countries,
+    //   countryISO: covidData[countryIndex].countryInfo.iso2,
+    //   countryName: covidData[countryIndex].country,
+    //   flag: covidData[countryIndex].countryInfo.flag,
+    //   cases: covidData[countryIndex].cases,
+    //   deaths: covidData[countryIndex].deaths,
+    //   recovered: covidData[countryIndex].recovered,
+    //   casesPerOneHundredThousand: Math.round(covidData[countryIndex].casesPerOneMillion / 10),
+    //   deathsPerOneHundredThousand: Math.round(covidData[countryIndex].deathsPerOneMillion / 10),
+    //   recoveredPerOneHundredThousand: Math.round(covidData[countryIndex].recoveredPerOneMillion
+    //     / 10),
+    //   todayCases: covidData[countryIndex].todayCases,
+    //   todayDeaths: covidData[countryIndex].todayDeaths,
+    //   todayRecovered: covidData[countryIndex].todayRecovered,
+    //   todayCasesPerOneHundredThousand: ((covidData[countryIndex].todayCases
+    //     / covidData[countryIndex].population) * 100000).toFixed(3),
+    //   todayDeathsPerOneHundredThousand: ((covidData[countryIndex].todayDeaths
+    //     / covidData[countryIndex].population) * 100000).toFixed(3),
+    //   todayRecoveredPerOneHundredThousand: ((covidData[countryIndex].todayRecovered
+    //     / covidData[countryIndex].population) * 100000).toFixed(3),
+    //   updated: (new Date(covidData[countryIndex].updated)).toDateString(),
+    // };
     // weight = 128KB
-    return covidDataPerCountries;
+    // return covidDataPerCountries;
+    return covidData;
   }
 
   async getTotalStats() {
     const url = this.urls.totalData;
     const covidData = await this.getDataFromUrl(url);
     const TotalCovidData = {
-      // total: covidData,
-      // iso2: covidData.countryInfo.iso2,
+      total: covidData,
       cases: covidData.cases,
       deaths: covidData.deaths,
       recovered: covidData.recovered,
@@ -97,34 +90,7 @@ export default class Stats {
     return TotalCovidData;
   }
 
-  async getWorldTimeline() {
-    const url = this.urls.worldTimeline;
-    const covidData = await this.getDataFromUrl(url);
-    const worldTimeline = {
-      cases: covidData.cases,
-      deaths: covidData.deaths,
-      recovered: covidData.recovered,
-    };
-    // weight = 17 KB
-    return worldTimeline;
-  }
-
-  async getCountryTimeline() {
-    const url = this.urls.countryTimeline;
-    const covidData = await this.getDataFromUrl(url);
-    // console.log(covidData);
-    const countryTimeline = {
-      covidData,
-      countryName: covidData.country,
-      cases: covidData.timeline.cases,
-      deaths: covidData.timeline.deaths,
-      recovered: covidData.timeline.recovered,
-    };
-    // weight = 15.2 KB
-    return countryTimeline;
-  }
-
-  async getTotalTimeline(countryName = 'Congo') {
+  async getTotalTimeline(countryName = null) {
     const url = this.urls.allCountriesTimeline;
     const covidData = await this.getDataFromUrl(url);
 
@@ -133,21 +99,46 @@ export default class Stats {
       for (let i = 0; i < Object.keys(covidData).length; i += 1) {
         countries[i] = covidData[i].country;
       }
-      return countries;
+      const result = new Set(countries);
+      return result;
     }
 
-    const countryIndex = getCountries().indexOf(countryName); // если нет в массиве, то -1
-
-    const allCountriesTimeline = {
+    let allCountriesTimeline = {
       covidData,
       countries: getCountries(),
-      countryName: covidData[countryIndex].country,
-      cases: covidData[countryIndex].timeline.cases,
-      deaths: covidData[countryIndex].timeline.deaths,
-      recovered: covidData[countryIndex].timeline.recovered,
-      fullDataForCountry: covidData[countryIndex],
     };
+    if (countryName) {
+      const countryIndex = getCountries().indexOf(countryName); // если нет в массиве, то -1
+
+      allCountriesTimeline = {
+        ...allCountriesTimeline,
+        ...{
+          countryName: covidData[countryIndex].country,
+          cases: covidData[countryIndex].timeline.cases,
+          deaths: covidData[countryIndex].timeline.deaths,
+          recovered: covidData[countryIndex].timeline.recovered,
+          fullDataForCountry: covidData[countryIndex],
+        },
+      };
+    }
     // weight = 3.5 MB
+    // return covidData;
     return allCountriesTimeline;
+  }
+
+  async prepareDataForMap() {
+    const totalData = await this.prepareTotalDataForMap();
+    const timelineData = await this.prepareAllCountriesTimelineForMap();
+    return [totalData, timelineData];
+  }
+
+  async prepareTotalDataForMap() {
+    this.totalData = await this.getTotalStats();
+    return this.totalData;
+  }
+
+  async prepareAllCountriesTimelineForMap() {
+    this.allCountriesTimeline = await this.getTotalTimeline();
+    // this.allCountriesTimeline.countries = this.allCountriesTimeline.countries.map();
   }
 }
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,63 @@
 import '../styles/style.scss';
+import isoCountries from './ISOCountries';
 
 import Stats from './Stats';
 import Map from './Map';
 
-class App {
+export default class App {
   constructor() {
-    const map = new Map();
+    this.stats = new Stats();
+    this.createCountriesList();
+  }
+
+  async init() {
+    const data = await this.stats.prepareDataForMap();
+    [this.totalData, this.allCountriesTimeline] = data;
+    this.totalData = await this.stats.getTotalStats();
+    this.allCountriesTimeline = await this.stats.getTotalTimeline();
+    this.map = new Map(this.totalData, this.allCountriesTimeline);
+    // console.log(this.allCountriesTimeline.covidData);
+    // console.log(this.totalData);
+  }
+
+  async createCountriesList() {
+    const countriesListEl = document.querySelector('#countriesList');
+    this.perCountryStats = await this.stats.getAllCountryStats();
+    // this.countriesList = Object.keys(isoCountries);
+    this.countriesList = Object.keys(this.perCountryStats);
+
+    console.log(this.perCountryStats[0]);
+    console.log(this.perCountryStats[0].active);
+
+    for (let i = 0; i < this.countriesList.length; i++) {
+      const countryTemplate = document.createElement('li');
+      countryTemplate.innerHTML = `
+        <li class="countries__item">
+          <span class="country__name">${this.perCountryStats[i].country}</span>
+          <span class="country__cases--All-Abs">${this.perCountryStats[i].cases}</span>
+          <span class="country__active--All-Abs">${this.perCountryStats[i].active}</span>
+          <span class="country__deaths--All-Abs">${this.perCountryStats[i].deaths}</span>
+          <span class="country__recovered--All-Abs">${this.perCountryStats[i].recovered}</span>
+
+          <span class="country__cases--Today-Abs">${this.perCountryStats[i].todayCases}</span>
+          <span class="country__deaths--Today-Abs">${this.perCountryStats[i].todayDeaths}</span>
+          <span class="country__recovered--Today-Abs">${this.perCountryStats[i].todayRecovered}</span>
+
+          <span class="country__cases--All-Per100k">${((this.perCountryStats[i].cases / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+          <span class="country__deaths--All-Per100k">${((this.perCountryStats[i].deaths / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+          <span class="country__active--All-Per100k">${((this.perCountryStats[i].active / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+          <span class="country__recovered--All-Per100k">${((this.perCountryStats[i].recovered / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+
+          <span class="country__cases--Today-Per100k">${((this.perCountryStats[i].todayCases / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+          <span class="country__deaths--Today-Per100k">${((this.perCountryStats[i].todayDeaths / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+          <span class="country__recovered--Today-Per100k">${((this.perCountryStats[i].todayRecovered / this.perCountryStats[i].population) * 100000).toFixed(3)}</span>
+          <img src="${this.perCountryStats[i].countryInfo.flag}" alt="" class="flag" width="auto" height="14">
+        </li>`;
+      countriesListEl.innerHTML += countryTemplate.innerHTML;
+    }
+    // console.log(Object.keys(isoCountries));
   }
 }
 
 const app = new App();
+app.init();
